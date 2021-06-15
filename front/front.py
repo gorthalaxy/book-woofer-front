e9a89eb786f3b59677f96108ec236c8f532134da
Steeve Roso <steeve.roso@gmail.com>
2021-06-15 05:36:03 -0400

    added columns

--- front/front.py
+++ front/front.py
@@ -5,55 +5,62 @@ from ebook import *
 import io
 from spotifywidget import select_and_play
 
+st.set_page_config(layout="wide")
+
 st.title('Book Woofer')
+
+
+
+col1, col2, = st.beta_columns((3,1))
 
 # uploaded_file = st.file_uploader("Choose an ebook")
 
-uploaded_file = st.file_uploader("Upload your kindle file.")
-temporary_location = False
-# param_object = 'I love apples'
-if uploaded_file is not None:
-  g = io.BytesIO(uploaded_file.read())  # BytesIO Object
-  temporary_location = "testout_simple.epub"
-  print(temporary_location)
-  with open(temporary_location, 'wb') as out:  # Open temporary file as bytes
-      out.write(g.read())  # Read bytes into file
-      output = read_book(temporary_location)
-      chapter = st.selectbox(
-          'Chapter:', [i+1 for i in range(len(output))])
-      param_object = [chapter-1]
-      st.markdown(output[chapter-1])
+with col1 :
+    uploaded_file = st.file_uploader("Upload your kindle file.")
+    temporary_location = False
+    # param_object = 'I love apples'
+    if uploaded_file is not None:
+        g = io.BytesIO(uploaded_file.read())  # BytesIO Object
+        temporary_location = "testout_simple.epub"
+        print(temporary_location)
+        with open(temporary_location, 'wb') as out:  # Open temporary file as bytes
+            out.write(g.read())  # Read bytes into file
+            output = read_book(temporary_location)
+            chapter = st.selectbox(
+                'Chapter:', [i+1 for i in range(len(output))])
+            param_object = [chapter-1]
+            st.markdown(output[chapter-1])
 
-input_object = st.text_input('Enter a sentence : ')
-if len(input_object) > 1:
-    param_object = [input_object]
+    input_object = st.text_input('Enter a sentence : ')
+    if len(input_object) > 1:
+        param_object = [input_object]
 
 #====================Send request and print prediction
 
-texts =  {'text' : param_object}
-url = "https://bfcontainer-csy3ocxwaq-ew.a.run.app/predict/"
-response = requests.post(
-    url,
-    params=texts,
-).json()
-st.text(response)
+    texts =  {'text' : param_object}
+    url = "https://bfcontainer-csy3ocxwaq-ew.a.run.app/predict/"
+    response = requests.post(
+        url,
+        params=texts,
+    ).json()
+    st.text(response)
 
 mood_colors = {'anger': 'B52525', 'fear': '489D38' , 'happy': 'DAC623', 'love': '9C37AA', 'neutral': '155249', 'sadness': '3298D5'}
 
-our_mood = max(response, key=response.get)
-select_and_play(our_mood)
-if response is not None:
-    st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: #{mood_colors[our_mood]}
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-st.text(our_mood)
+# our_mood = max(response, key=response.get)
+# select_and_play(our_mood)
+# if response is not None:
+#     st.markdown(
+#     f"""
+#     <style>
+#     .reportview-container {{
+#         background: #{mood_colors[our_mood]}
+#     }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+# st.text(our_mood)
 
 #plot your prediction
 
@@ -64,7 +71,20 @@ st.text(our_mood)
 #     height=600
 # )
 
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-plt.bar(x = response.keys(), height = response.values())
-st.pyplot()
+with col2:
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    plt.bar(x = response.keys(), height = response.values())
+    st.pyplot()
+    our_mood = max(response, key=response.get)
+    select_and_play(our_mood)
+    if response is not None:
+        st.markdown(
+        f"""
+        <style>
+        .reportview-container {{
+            background: #{mood_colors[our_mood]}
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
